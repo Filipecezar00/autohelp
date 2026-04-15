6e05b3a36157a8cd5b086382b00a7732a358d296
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-15 11:43:04 -0300

    atualizando

--- backend/src/controllers/prestadores.controller.js
+++ backend/src/controllers/prestadores.controller.js
@@ -11,3 +11,23 @@ const listar = async (req, res) => {
     res.status(500).json({ mensagem: "Erro ao buscar prestadores" });
   }
 };
+
+const buscarPorId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM prestadores WHERE id = ?", [
+      id,
+    ]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ mensagem: "Prestador não encontrado" });
+    }
+
+    res.json(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao realizar busca do prestador de serviço" });
+  }
+};
