01686a2b9ac280ed7f28bb365d2a5ead5358d92e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-15 22:48:01 -0300

    atualizando

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const prestadoresRoutes = require("./src/routes/prestadores.routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/prestadores", prestadoresRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API Funcionando!" });
