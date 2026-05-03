d531badbc390225c6583f968c89a16157a059202
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-03 19:40:21 -0300

    atualizando

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -23,9 +23,12 @@ api.interceptors.response.use((response) => {
       if (error.response && error.response.status === 401) {
         localStorage.removeItem("token");
         localStorage.removeItem("usuario");
+
+        if (window.location.pathname !== "/login") {
+          window.location.href = "/login";
+        }
       }
-      window.location.href = "/login";
+      return Promise.reject(error);
     });
-  return Promise.reject(error);
 });
 export default api;
