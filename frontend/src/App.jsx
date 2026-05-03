6cacc8c31f8b0abadba6b3490e8a31793e4052a2
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-03 20:29:36 -0300

    atualizando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,33 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
-import { Suspense } from "react";
+import PrivateRoute from "./components/PrivateRoute";
+import AuthProvider from "./contexts/AuthContext";
+import { Suspense, lazy } from "react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div className="loading-spinner">Carregando...</div>}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />}></Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/home" element={<Home />} />
-          <Route
-            path="*"
-            element={<h2>Erro 404: Página não encontrada</h2>}
-          ></Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Suspense
+          fallback={<div className="loading-spinner">Carregando...</div>}
+        >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+
+            <Route element={<PrivateRoute />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route path="/" element={<Navigate to="/login" replace />}></Route>
+            <Route
+              path="*"
+              element={<h2>Erro 404: Página não encontrada</h2>}
+            ></Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
