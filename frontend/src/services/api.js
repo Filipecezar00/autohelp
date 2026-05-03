451bf9cf2b3b6a7e3d852e234e1c7d027c14b700
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-03 19:04:23 -0300

    atualizando

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,12 +4,28 @@ const api = axios.create({
   baseURL: "http://localhost:3001/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = "Bearer " + token;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
+api.interceptors.response.use((response) => {
+  (response,
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("usuario");
+      }
+      window.location.href = "/login";
+    });
+  return Promise.reject(error);
+});
 export default api;
