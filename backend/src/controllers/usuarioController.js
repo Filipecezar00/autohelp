7cebaa1c8085569471717ada177e2c8bc4648325
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-05 23:12:16 -0300

    atualizando

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -84,4 +84,69 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { cadastrarUsuario, login };
+const buscarPrestadoresPorDistancia = async (req, res) => {
+  try {
+    let lat = parseFloat(req.query.lat);
+    let lng = parseFloat(req.query.lng);
+    let raio = parseFloat(req.query.raio) || 10;
+
+    if (isNaN(lat) || isNaN(lng)) {
+      return res
+        .json({ error: "Latitude e longitude são obrigatórios" })
+        .status(400);
+    }
+    if (lat < -90 || lat > 90 || lng < -180 || lng > 180) {
+      return res.json({ error: "Coordenadas inválidas" }).status(400);
+    }
+
+    const query = `SELECT prestadores.*, usuarios.nome FROM prestadores JOIN usuarios ON prestadores.usuario_id = usuarios.id WHERE prestadores.ativo = verdadeiro AND prestadores.latitude IS NOT NULL AND prestadores.longitude IS NOT NULL`;
+
+    const prestadores = [];
+    const converterRadianos = (graus) => graus * (Math.PI / 180);
+
+    const calcularDistanciaHaversine = (lat1, lon1, lat2, lon2) => {
+      const raioTerra = 6371;
+
+      let dlat = converterRadianos(lat2 - lat1);
+      let dlon = converterRadianos(lon2 - lon1);
+
+      let lat1Rad = converterRadianos(lat1);
+      let lat2Rad = converterRadianos(lat2);
+
+      let a =
+        Math.sin(dlat / 2) * Math.sin(dlng / 2) +
+        Math.sin(dlon / 2) *
+          Math.sin(dlon / 2) *
+          Math.cos(lat1Rad) *
+          Math.cos(lat2Rad);
+      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      return raioTerra * c;
+    };
+
+    const prestadorComDistancia = prestadores.map((prestador) => {
+      const distancia = calcularDistanciaHaversine(
+        lat,
+        lng,
+        parseFloat(prestador.lat),
+        parseFloat(prestador.lng),
+      );
+      return { ...prestador, distancia_km: Number(distancia.toFixed(1)) };
+    });
+    const dentroDoRaio = prestadorComDistancia.filter(
+      (p) => p.distancia_km <= raio,
+    );
+
+    const ordenados = dentroDoRaio.sort(
+      (a, b) => a.distancia_km - b.distancia_km,
+    );
+
+    return res.status(200).json(ordenados);
+  } catch (error) {
+    console.error("Erro em Buscar Prestadores por distancia:", error);
+    return res
+      .status(500)
+      .json({ error: "Erro interno ao buscar prestadores" });
+  }
+};
+
+module.exports = { cadastrarUsuario, login, buscarPrestadoresPorDistancia };
