993a725dff7d724e5b92031fe675f4b8c1d70674
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-16 14:07:41 -0300

    atualizando

--- backend/src/routes/prestadores.routes.js
+++ backend/src/routes/prestadores.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const prestadoresController = require("../controllers/prestadores.controller.js");
+const {
+  buscarPrestadoresPorDistancia,
+} = require("../controllers/usuarioController.js");
 
 router.get("/", prestadoresController.listar);
-router.get("/:id", prestadoresController.buscarPorId);
 router.post("/", prestadoresController.criar);
+router.get("/proximos", buscarPrestadoresPorDistancia);
+router.get("/:id", prestadoresController.buscarPorId);
 
 module.exports = router;
