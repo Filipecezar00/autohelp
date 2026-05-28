cbe45b59004e227f75427bc516ac034d861c9cb1
Filipecezar00 <cezarfilipe01@gmail.com>
2026-05-28 11:01:14 -0300

    atualizando

---
+++ frontend/src/pages/Solicitacao.jsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { TelaCarregando } from "../components/TelaCarregando";
+import { TelaErro } from "../components/TelaErro";
+import { useNavigate } from "react-router-dom";
+import { GiConfirmed } from "react-icons/gi";
+
+import api from "../services/api";
+export default async function Solicitacao() {
+  const { prestadorId } = useParams();
+
+  const [prestador, setPrestador] = useState(null);
+  const [descricao, setDescricao] = useState("");
+  const [enviando, setEnviando] = useState(false);
+  const [erro, setErro] = useState(null);
+  const [sucesso, setSucesso] = useState(false);
+  const [carregando, setCarregando] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    try {
+      const resposta = api.get("/prestadores/" + prestadorId);
+      setPrestador(resposta.dados);
+    } catch {
+      setErro("Prestador não encontrado");
+    } finally {
+      setCarregando(false);
+    }
+  }, []);
+
+  const feedBackTexto = (e) => {
+    setDescricao(e.target.value);
+  };
+
+  const IrParaHistorico = () => {
+    navigate("/historico");
+  };
+
+  const IrParaMapa = () => {
+    navigate("/mapa");
+  };
+
+  async function handleSubmit() {
+    if (!descricao.trim()) {
+      setErro("Descreva o Problema para continuar");
+      return;
+    }
+    if (descricao.length < 10) {
+      setErro("Descrição muito curta - detalhe um Pouco mais");
+      return;
+    }
+
+    try {
+      setEnviando(true);
+      setErro(null);
+
+      await api.post("/solicitacoes", {
+        prestador_id: prestadorId,
+        descricao: descricao.trim(),
+      });
+
+      setSucesso(true);
+    } catch (erro) {
+      if (erro.response.status === 409) {
+        setErro("Você já tem uma solicitação ativa com esse prestador.");
+      } else if (erro) {
+        setErro("Erro ao enviar solicitação. Tente novamente");
+      }
+    } finally {
+      setEnviando(false);
+    }
+  }
+  if (carregando) {
+    return <TelaCarregando />;
+  }
+  if (erro && prestador === null) {
+    return <TelaErro message={"Prestador não encontrado"} />;
+  }
+
+  if (sucesso) {
+    return (
+      <div>
+        <GiConfirmed />
+        <p>Solicitação enviada com Sucesso!</p>
+        <p>O Prestador foi notificado e entrará em contato.</p>
+        <button onClick={IrParaHistorico}>Ver minhas solicitações</button>
+        <button onClick={IrParaMapa}>Voltar ao Mapa</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>
+          <h1>{prestador.nome}</h1>
+          <p>{prestador.tipo}</p>
+          <p>{prestador.telefone}</p>
+          <p>{prestador.distancia}</p>
+        </div>
+        <textarea
+          placeholder="Ex: pneu furado na rua X, próximo ao Y"
+          onChange={feedBackTexto}
+          value={descricao}
+          required
+        >
+          Descreva com detalhes seu Problema
+        </textarea>
+        <small>Caracteres Digitados:{descricao.length}</small>
+        {erro && <p>Verifique o comprimento e tente novamente.</p>}
+        <button disabled={enviando} onClick={handleSubmit}>
+          {enviando ? "Enviando" : "Enviar Solicitação"}
+        </button>
+      </div>
+    );
+  }
+}
