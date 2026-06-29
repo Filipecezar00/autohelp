5c4992d154ea74501bdea44abee8a7976ec00663
Filipecezar00 <cezarfilipe01@gmail.com>
2026-06-29 11:15:14 -0300

    atualizando

--- backend/src/controllers/perfil.controller.js
+++ backend/src/controllers/perfil.controller.js
@@ -116,3 +116,59 @@ export async function trocarSenha(req, res) {
     res.status(500).json({ message: "Erro ao realizar mudança de senha" });
   }
 }
+
+export async function deletarConta(req, res) {
+  try {
+    const usuarioId = req.user.id;
+    const { senha } = req.body;
+
+    const [usuarios] = await pool.query(
+      `SELECT senha FROM usuarios WHERE id = ?`,
+      [usuarioId],
+    );
+
+    const usuario = usuarios[0];
+
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+
+    if (usuarios.length === 0) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    if (!senhaCorreta) {
+      return res.status(401).json({ message: "Senha Incorreta" });
+    }
+    await pool.query(
+      `
+    DELETE FROM mensagens WHERE usuario_id = ?
+    `,
+      [usuarioId],
+    );
+
+    await pool.query(
+      `
+    DELETE FROM solicitacoes WHERE cliente_id = ? 
+    `,
+      [usuarioId],
+    );
+
+    await pool.query(
+      `
+    DELETE FROM solicitacoes WHERE prestador_id =
+    (SELECT id FROM prestadores WHERE usuario_id = ?)
+    `,
+      [usuarioId],
+    );
+
+    await pool.query(`DELETE FROM prestadores WHERE usuario_id = ?`, [
+      usuarioId,
+    ]);
+
+    await pool.query(`DELETE FROM usuarios WHERE id = ?`, [usuarioId]);
+
+    return res.status(200).json({ message: "Conta deletada com Sucesso" });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao realizar exclusão da conta" });
+    console.error(`ERRO AO REALIZAR EXCLUSÃO DA CONTA:`, error);
+  }
+}
