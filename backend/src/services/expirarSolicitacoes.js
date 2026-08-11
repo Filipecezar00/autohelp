5cdf32fcbb3e62301e3e9c458667145e2615f43e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-11 11:56:08 -0300

    atualizando

--- backend/src/services/expirarSolicitacoes.js
+++ backend/src/services/expirarSolicitacoes.js
@@ -1,5 +1,6 @@
 import cron from "node-cron";
 import pool from "../config/database";
+import { io } from "../../server";
 
 export async function iniciarJob() {
   const frequenciaCron = "*/1 * * * *";
@@ -22,4 +23,40 @@ export async function iniciarJob() {
       console.log("ERRO AO EXECUTAR CRON:", error);
     }
   });
+}
+
+export async function processarExpiracaoEmLote(io) {
+  try {
+    const [pendentesVencidas] = await pool.query(
+      `SELECT id,cliente_id,prestador_id FROM solicitacoes
+       WHERE status = 'pendente' AND criado_em <=NOW() - INTERVAL 30 MINUTE`,
+    );
+    if (pendentesVencidas.length === 0) {
+      return;
+    }
+    const listaDeIds = pendentesVencidas.map((item) => item.id);
+
+    await pool.query(
+      `UPDATE solicitacoes SET status='expirado' WHERE id IN(?)`,
+      [listaDeIds],
+    );
+
+    pendentesVencidas.forEach((item) => {
+      const informacaoEvento = {
+        solicitacaoId: item.id,
+        status: "Expirado",
+        mensagem: "A solicitação expirou por tempo limite",
+      };
+
+      const canalCliente = "usuario_" + item.cliente_id;
+      io.to(canalCliente).emit("solicitacao_expirada", informacaoEvento);
+      if (item.prestador_id != null) {
+        const canalPrestador = "usuario_" + item.prestador_id;
+        io.to(canalPrestador).emit("solicitacao_expirada", informacaoEvento);
+      }
+    });
+    console.log("Notificações de expiração enviadas com sucesso!");
+  } catch (error) {
+    console.error("ERRO AO PROCESSAR SOLICITAÇÕES EXPIRADAS:", error);
+  }
 }
