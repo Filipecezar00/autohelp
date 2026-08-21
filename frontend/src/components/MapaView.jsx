7278cca5806a0bc7f8c9c4796ba53070ef0ba1ac
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-21 15:13:37 -0300

    atualizando

--- frontend/src/components/MapaView.jsx
+++ frontend/src/components/MapaView.jsx
@@ -327,6 +327,7 @@ export default function MapaView({
                           `/solicitar/${prestador.usuario_id}`,
                           {
                             state: {
+                              prestador_id_tabela: prestador.prestador_id,
                               nome: prestador.nome,
                               distancia: prestador.distancia_km,
                             },
