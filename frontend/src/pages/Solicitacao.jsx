361eef0637fb28c11127688128a10286a87cab60
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-21 00:09:06 -0300

    atualizando

--- frontend/src/pages/Solicitacao.jsx
+++ frontend/src/pages/Solicitacao.jsx
@@ -70,6 +70,7 @@ export default function Solicitacao() {
       setEnviando(true);
       setErro(null);
 
+      console.log("ID PEGO NA URL:", prestadorId);
       const resposta = await api.post("/solicitacoes", {
         prestador_id: prestadorId,
         descricao: descricao.trim(),
